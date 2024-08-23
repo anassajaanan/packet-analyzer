0821de0e363c8f31aabfc597f145029be6ad0295
Anas Ajaanan <p131047727@taalim.ma>
2024-08-23 12:24:51 +0400

    Add the hashing function

--- connection_tracker.c
+++ connection_tracker.c
@@ -16,6 +16,11 @@ typedef struct hash_entry {
 static hash_entry *hash_table[HASH_SIZE];
 
 
+static uint32_t hash_function(uint32_t src_ip, uint32_t dst_ip, uint16_t src_port, uint16_t dst_port) {
+    return (src_ip ^ dst_ip ^ ((uint32_t)src_port << 16 | dst_port)) % HASH_SIZE;
+}
+
+
 void init_connection_tracker() {
     memset(hash_table, 0, sizeof(hash_table));
 }
